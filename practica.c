2ad2d538455cb1d9af44fae914c810aee685b6ca
dmarts05 <dmarts05@estudiantes.unileon.es>
2022-12-24 02:24:49 +0100

    Agregar variables condición y arrays de trabajadores
    Se ha preparado la aplicación para cambiar el número de trabajadores (aún no se han implementado punteros). Se ha mejorado el proceso de creación de hilos con bucles for. Se ha agregado la variable condición restante asociada a las solicitudes de atención domiciliaria. Ahora los mutex se eliminan antes de salir de la aplicación. Los argumentos pasados a los métodos de los hilos ahora se pasan correctamente.

--- practica.c
+++ practica.c
@@ -7,36 +7,43 @@
 #include <signal.h>
 
 #define numClientes 20
-
+#define numTecnicos 2
+#define numRespReparaciones 2
+#define numEncargados 1
+#define numTecAttDomiciliaria 1
 
 /**DECLARACIONES GLOBALES*/
 
-//Falta declarar las variables condición
-
+// Mutex
 pthread_mutex_t Fichero, colaClientes, solicitudes;
 
-int contadorApp, contadorRed;
+// Variables condición
+pthread_cond_t condSolicitudesDomicilio;
 
-int numSolicDomicilio;
+// Contadores
+int contadorApp, contadorRed, numSolicitudesDomicilio;
 
-struct Clientes {
+struct Clientes
+{
 	int id;
-	int atendido; 	//0 SI ATENDIDO; 1 SI ESTÁ EN PROCESO; 2 SI ESTÁ ATENDIDO
-	int tipo;		// 0 SI APP; 1 SI RED
+	int atendido; // 0 SI ATENDIDO; 1 SI ESTÁ EN PROCESO; 2 SI ESTÁ ATENDIDO
+	int tipo;	  // 0 SI APP; 1 SI RED
 	int prioridad;
 	int solicitud;
-
 };
 
-
-int tecnico[2];		//REVISAR, NO ESTÁ BIEN
+// Listas de hilos trabajadores
+pthread_t tecnicos[numTecnicos];
+pthread_t respReparaciones[numRespReparaciones];
+pthread_t encargados[numEncargados];
+pthread_t tecAttDomiciliaria[numTecAttDomiciliaria];
 
 FILE *logFile;
 
-
 /**FUNCIONES*/
 
-void writeLogMessage(char *id, char *msg) {
+void writeLogMessage(char *id, char *msg)
+{
 	//	Calculamos la hora actual
 	time_t now = time(0);
 	struct tm *tlocal = localtime(&now);
@@ -44,7 +51,6 @@ void writeLogMessage(char *id, char *msg) {
 	strftime(stnow, 25, "%d/%m/%y %H:%M:%S", tlocal);
 
 	// Escribimos en el log
-
 	logFile = fopen("registroTiempos.log", "a");
 	fprintf(logFile, "[%s] %s: %s\n", stnow, id, msg);
 	fclose(logFile);
@@ -52,124 +58,221 @@ void writeLogMessage(char *id, char *msg) {
 
 /**MANEJADORAS DE SEÑAL*/
 
-void handlerClienteApp(int sig){
+void handlerClienteApp(int sig)
+{
 	printf("Hola SIGUSR1\n");
 }
 
-void handlerClienteRed(int sig){
+void handlerClienteRed(int sig)
+{
 	printf("Hola SIGUSR2\n");
 }
 
-void handlerTerminar(int sig){
+void handlerTerminar(int sig)
+{
 	printf("Hola SIGINT\n");
-	exit(0);		//ESTO SOBRA, ES PARA SALIR POR AHORA
+	exit(0); // ESTO SOBRA, ES PARA SALIR POR AHORA
 }
 
 /**CÓDIGOS DE EJECUCIÓN DE HILOS*/
 
-void *Tecnico(void *arg) {
-	printf("Técnico %d\n", (int*) arg);
+void *Tecnico(void *arg)
+{
+	int index = *(int *)arg;
+	printf("Técnico %d\n", index);
+	free(arg);
 }
 
-void *Responsable(void *arg) {
-	printf("Responsable %d\n", (int*) arg);
+void *Responsable(void *arg)
+{
+	int index = *(int *)arg;
+	printf("Responsable %d\n", index);
+	free(arg);
 }
 
-void *Encargado(void *arg) {
-	printf("Encargado\n");
+void *Encargado(void *arg)
+{
+	int index = *(int *)arg;
+	printf("Encargado %d\n", index);
+	free(arg);
 }
 
-void *AtencionDomiciliaria(void *arg) {
-	printf("Atención domiciliaria\n");
+void *AtencionDomiciliaria(void *arg)
+{
+	int index = *(int *)arg;
+	printf("Atención domiciliaria %d\n", index);
+	free(arg);
 }
-
-
 
 /**MAIN*/
 
-int main(){
+int main()
+{
 	struct sigaction sig;
 	int i;
-	sig.sa_handler=handlerClienteApp;
-	if(sigaction(SIGUSR1, &sig, NULL)==-1){
-		perror("[ERROR] Error en la llamada a sigaction");
+	sig.sa_handler = handlerClienteApp;
+	if (sigaction(SIGUSR1, &sig, NULL) == -1)
+	{
+		perror("[ERROR] Error en la llamada a sigaction.");
 		exit(-1);
 	}
 
-	sig.sa_handler=handlerClienteRed;
-	if(sigaction(SIGUSR2, &sig, NULL)==-1){
-		perror("[ERROR] Error en la llamada a sigaction");
+	sig.sa_handler = handlerClienteRed;
+	if (sigaction(SIGUSR2, &sig, NULL) == -1)
+	{
+		perror("[ERROR] Error en la llamada a sigaction.");
 		exit(-1);
 	}
 
-	sig.sa_handler=handlerTerminar;
-	if(sigaction(SIGINT, &sig, NULL)==-1){
-		perror("[ERROR] Error en la llamada a sigaction");
+	sig.sa_handler = handlerTerminar;
+	if (sigaction(SIGINT, &sig, NULL) == -1)
+	{
+		perror("[ERROR] Error en la llamada a sigaction.");
 		exit(-1);
 	}
 
 	/**INICIALIZAR RECURSOS*/
+
+	// Inicicialización de mutex
 	pthread_mutex_init(&Fichero, NULL);
 	pthread_mutex_init(&colaClientes, NULL);
 	pthread_mutex_init(&solicitudes, NULL);
 
+	// Inicialización de variables condición
+	pthread_cond_init(&condSolicitudesDomicilio, NULL);
+
+	// Inicialización de contadores
 	contadorApp = 0;
 	contadorRed = 0;
+	numSolicitudesDomicilio = 0;
 
-	struct Clientes listaclientes[numClientes];
-	for(i=0; i<numClientes;i++){
-		listaclientes[i].id = 0;
-		listaclientes[i].prioridad = 0;
-		listaclientes[i].atendido = 0;
-		listaclientes[i].solicitud = 0;
+	struct Clientes listaClientes[numClientes];
+	for (i = 0; i < numClientes; i++)
+	{
+		listaClientes[i].id = 0;
+		listaClientes[i].prioridad = 0;
+		listaClientes[i].atendido = 0;
+		listaClientes[i].solicitud = 0;
 	}
 
-	tecnico[0] = 0;
-	tecnico[1] = 0;
+	// Inicialización de técnicos
+	for (i = 0; i < numTecnicos; i++)
+	{
+		int *index = malloc(sizeof(int));
+		*index = i + 1;
+		if (pthread_create(&tecnicos[i], NULL, &Tecnico, index) != 0)
+		{
+			perror("[ERROR] Error al crear hilo de técnico.");
+			return -1;
+		}
+	}
 
-	pthread_t tecnico_1, tecnico_2, resprep_1, resprep_2, encargado, attdomicil;
+	// Inicialización de responsables de reparaciones
+	for (i = 0; i < numRespReparaciones; i++)
+	{
+		int *index = malloc(sizeof(int));
+		*index = i + 1;
+		if (pthread_create(&respReparaciones[i], NULL, &Responsable, index) != 0)
+		{
+			perror("[ERROR] Error al crear hilo de responsable de reparaciones.");
+			return -1;
+		}
+	}
 
-	pthread_create(&tecnico_1, NULL, Tecnico, (int*)1);
-	pthread_create(&tecnico_2, NULL, Tecnico, (int*)2);
-	pthread_create(&resprep_1, NULL, Responsable, (int*) 1);
-	pthread_create(&resprep_2, NULL, Responsable, (int*) 2);
-	pthread_create(&encargado, NULL, Encargado, NULL);
-	pthread_create(&attdomicil, NULL, AtencionDomiciliaria, NULL);
-	
+	// Inicialización de encargados
+	for (i = 0; i < numEncargados; i++)
+	{
+		int *index = malloc(sizeof(int));
+		*index = i + 1;
+		if (pthread_create(&encargados[i], NULL, &Encargado, index) != 0)
+		{
+			perror("[ERROR] Error al crear hilo de encargado.");
+			return -1;
+		}
+	}
 
+	// Inicialización de técnicos de atención domiciliaria
+	for (i = 0; i < numTecAttDomiciliaria; i++)
+	{
+		int *index = malloc(sizeof(int));
+		*index = i + 1;
+		if (pthread_create(&tecAttDomiciliaria[i], NULL, &AtencionDomiciliaria, index) != 0)
+		{
+			perror("[ERROR] Error al crear hilo de técnico de atención domiciliaria.");
+			return -1;
+		}
+	}
 
-	while(1){
+	while (1)
+	{
 		pause();
 	}
-		
+
+	// IMPORTANTE: No sé si esto es necesario, conviene revisarlo posteriormente - Joins de todos los hilos
+	// for (i = 0; i < numTecnicos; i++)
+	// {
+	// 	if (pthread_join(tecnicos[i], NULL) != 0)
+	// 	{
+	// 		perror("[ERROR] Error al unir hilo de técnico.");
+	// 		return -1;
+	// 	}
+	// }
+
+	// for (i = 0; i < numRespReparaciones; i++)
+	// {
+	// 	if (pthread_join(respReparaciones[i], NULL) != 0)
+	// 	{
+	// 		perror("[ERROR] Error al unir hilo de responsable de reparaciones.");
+	// 		return -1;
+	// 	}
+	// }
+
+	// for (i = 0; i < numEncargados; i++)
+	// {
+	// 	if (pthread_join(encargados[i], NULL) != 0)
+	// 	{
+	// 		perror("[ERROR] Error al unir hilo de encargado.");
+	// 		return -1;
+	// 	}
+	// }
+
+	// for (i = 0; i < numTecAttDomiciliaria; i++)
+	// {
+	// 	if (pthread_join(tecAttDomiciliaria[i], NULL) != 0)
+	// 	{
+	// 		perror("[ERROR] Error al unir hilo de técnico de atención domiciliaria.");
+	// 		return -1;
+	// 	}
+	// }
+
+	// Eliminar los mutex al salir
+	pthread_mutex_destroy(&Fichero);
+	pthread_mutex_destroy(&colaClientes);
+	pthread_mutex_destroy(&solicitudes);
+
 	return 0;
 }
 
-
-//GUILLERMO
-void nuevoCliente(){
-
+// GUILLERMO
+void nuevoCliente()
+{
 }
 
-
-//ÁLVARO
-void accionesCliente(){
-
+// ÁLVARO
+void accionesCliente()
+{
 }
 
-//DANIEL
-void accionesTecnico(){
-
+// DANIEL
+void accionesTecnico()
+{
 }
 
-
-//MARIO
-void accionesEncargado(){
-
+// MARIO
+void accionesEncargado()
+{
 }
 
-
-void accionesTecnicoDomiciliario(){
-
+void accionesTecnicoDomiciliario()
+{
 }
-
