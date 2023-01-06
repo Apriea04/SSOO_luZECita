240a5377801d9c3fef5aa88c7fead3a50f5cae57
MarioLopB <mlopeb04@estudiantes.unileon.es>
2023-01-06 15:28:03 -0500

    Cambiar condicion ambos argumentos

--- argumentos.sh
+++ argumentos.sh
@@ -48,7 +48,7 @@ read clientes
 echo "Número máximo de técnicos"
 read tecnicos
 
-if [[ $clientes =~ $es_numero ]] || [[ $tecnicos =~ $es_numero ]]
+if [[ $clientes =~ $es_numero ]] && [[ $tecnicos =~ $es_numero ]]
 then
 ./practica --clientes $clientes --tecnicos $tecnicos
 exit 0
